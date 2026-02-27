8ed01e5d49eb771a9f648a21bbca84baa3351513
Khai Phan <khai.phan@datadoghq.com>
2026-02-27 20:04:53 +0000

    Update setup-pyenv.sh

--- scripts/setup-pyenv.sh
+++ scripts/setup-pyenv.sh
@@ -9,6 +9,18 @@ else
   echo "pyenv already installed"
 fi
 
+# Add pyenv shell integration to .zshrc if not already present
+if ! grep -q 'PYENV_ROOT' ~/.zshrc 2>/dev/null; then
+  echo '' >> ~/.zshrc
+  echo '# pyenv' >> ~/.zshrc
+  echo 'export PYENV_ROOT="$HOME/.pyenv"' >> ~/.zshrc
+  echo '[[ -d $PYENV_ROOT/bin ]] && export PATH="$PYENV_ROOT/bin:$PATH"' >> ~/.zshrc
+  echo 'eval "$(pyenv init - zsh)"' >> ~/.zshrc
+  echo "pyenv shell integration added to ~/.zshrc"
+else
+  echo "pyenv shell integration already in ~/.zshrc"
+fi
+
 # Set up shell environment for the current session
 export PYENV_ROOT="$HOME/.pyenv"
 [[ -d $PYENV_ROOT/bin ]] && export PATH="$PYENV_ROOT/bin:$PATH"
